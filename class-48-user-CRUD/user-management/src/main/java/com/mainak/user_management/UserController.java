5bf1216f474d6dda949a7632d5eeca5729f8c3ee
Mainak <mainakgh1@gmail.com>
2026-09-21 23:43:27 +0530

    springboot-apis-example

---
+++ class-48-user-CRUD/user-management/src/main/java/com/mainak/user_management/UserController.java
@@ -0,0 +1,37 @@
+package com.mainak.user_management;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    UserService userService = new  UserService();
+
+
+    // GET /users
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("users/{id}")
+    public User getUser(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+
+    @GetMapping("users/search")
+    public List<User> searchUsers(@RequestParam int age) {
+        return userService.getUsersByAge(age);
+    }
+
+    // add a user
+    @PostMapping("/user")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+}
---
+++ class-48-user-CRUD/user-management/src/main/java/com/mainak/user_management/UserService.java
@@ -0,0 +1,70 @@
+package com.mainak.user_management;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+
+    private List<User> users = new ArrayList<>();
+
+    public UserService() {
+        users.add(new User(1, "John", 25));
+        users.add(new User(2, "Alice", 30));
+        users.add(new User(3, "Bob", 22));
+    }
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    public User getUserById(int id) {
+
+        for (User user : users) {
+
+            if (user.getId() == id) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void updateUser(int id, User updatedUser) {
+
+        User u1 = getUserById(id);
+        if (u1 != null) {
+            deleteUser(id);
+            addUser(updatedUser);
+        }
+
+    }
+
+    public void deleteUser(int id) {
+
+        for (int i = 0; i < users.size(); i++) {
+
+            if (users.get(i).getId() == id) {
+                users.remove(i);
+                return;
+            }
+        }
+    }
+
+    public List<User> getUsersByAge(int age) {
+
+        List<User> result = new ArrayList<>();
+
+        for (User user : users) {
+
+            if (user.getAge() == age) {
+                result.add(user);
+            }
+        }
+
+        return result;
+    }
+}
